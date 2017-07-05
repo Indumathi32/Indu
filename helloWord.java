ddf39cea825538f0b3bd98d502bcddda79e3e060
Indumathi32 <Indumathisagunth@gmail.com>
2017-07-05 11:47:26 +0530

    Add files via upload

--- helloWord.java
+++ helloWord.java
@@ -5,11 +5,8 @@ public class helloWord {
 	public static void main(String[] args) {
 		int i;
 		for(i=0;i<=5;i++)
-		{
-		
-			System.out.println("hello");
-		}
-
+		System.out.println("hello");
+        
 	}
 
 }

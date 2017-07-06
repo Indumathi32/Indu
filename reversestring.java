e3b9aeeb0fa52ffb3b67a14920f88d3f0eb993c6
Indumathi32 <Indumathisagunth@gmail.com>
2017-07-06 17:11:49 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,18 @@
+package value;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+public class reversestring 
+{
+  public static void main(String[] args)
+  {
+     String input = "GeeksforGeeks";
+     byte [] strAsByteArray = input.getBytes();
+	 byte [] result = new byte [strAsByteArray.length];
+	 for (int i = 0; i<strAsByteArray.length; i++)
+	 result[i] = strAsByteArray[strAsByteArray.length-i-1];
+	 System.out.println(new String(result));
+	}
+}
+
+

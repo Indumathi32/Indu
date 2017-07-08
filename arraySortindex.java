a2e0a97c0b34bbf41fefc655064e1e62f46f4dcf
Indumathi32 <Indumathisagunth@gmail.com>
2017-07-08 11:59:59 +0530

    Add files via upload

---
+++ arraySortindex.java
@@ -0,0 +1,25 @@
+package value;
+import java.util.Scanner;
+public class arraySortindex 
+{
+  public static void main(String[] args)
+  {
+	Scanner sc=new Scanner(System.in);
+	int n1=sc.nextInt();
+	int a[]=new int[n1];
+	for(int i=0;i<n1;i++)
+	{
+	a[i]=sc.nextInt();
+	}
+	for(int i=0;i<n1;i++)
+	{
+	if(a[i]==i)
+	{
+	System.out.println(a[i]);
+	}
+	}
+  }
+}
+	
+
+

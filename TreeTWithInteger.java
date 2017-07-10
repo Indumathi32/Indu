63716a83e90776450f7c2f7e16a1d793ab398e19
Indumathi32 <Indumathisagunth@gmail.com>
2017-07-10 12:44:13 +0530

    Add files via upload

---
+++ TreeTWithInteger.java
@@ -0,0 +1,18 @@
+package value;
+import java.util.Scanner;
+public class TreeTWithInteger
+{
+  private static final String String = null;
+  public static String main(String[] args) 
+  {
+     Scanner sc=new Scanner(System.in);
+     String str;
+     System.out.println("ENETR YOUR STRING=\n");
+     str=sc.nextLine();
+     if (str.length() <= 1) return str;
+     String mid = str.substring(1, str.length()-1);
+     return str.charAt(str.length()-1) + mid + str.charAt(0);
+   }
+}
+
+

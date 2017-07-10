78b72f681db7ddae45fccb4bc8fb29a89b8f59c4
Indumathi32 <Indumathisagunth@gmail.com>
2017-07-10 14:33:21 +0530

    Add files via upload

---
+++ Players.java
@@ -0,0 +1,17 @@
+package value;
+import java.util.Scanner;
+public class Players 
+{
+  public static void main(String[] args)
+  {
+    int a,b,c;
+    System.out.println("Enter the first number");
+	Scanner s=new Scanner(System.in);
+	a=s.nextInt();
+	System.out.println("Enter the second number");
+	b=s.nextInt();
+	System.out.println("The multiplication is");
+	c=a*b;
+	System.out.println(c);
+  }
+}
